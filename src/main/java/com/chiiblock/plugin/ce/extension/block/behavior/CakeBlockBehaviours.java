535abbd2c8118f6a349e3244f1ac04cc6f07c3e5
iqtester <1835ww@gmail.com>
2025-10-11 18:13:24 -0400

    最后改动

--- src/main/java/com/chiiblock/plugin/ce/extension/block/behavior/CakeBlockBehaviours.java
+++ src/main/java/com/chiiblock/plugin/ce/extension/block/behavior/CakeBlockBehaviours.java
@@ -2,6 +2,15 @@ package com.chiiblock.plugin.ce.extension.block.behavior;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import io.lumine.mythic.api.adapters.AbstractLocation;
+import io.lumine.mythic.api.mobs.MythicMob;
+import io.lumine.mythic.bukkit.BukkitAdapter;
+import io.lumine.mythic.bukkit.MythicBukkit;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.title.Title;
 import net.momirealms.craftengine.bukkit.api.CraftEngineBlocks;
 import net.momirealms.craftengine.bukkit.block.behavior.BukkitBlockBehavior;
 import net.momirealms.craftengine.core.block.BlockBehavior;
@@ -11,9 +20,12 @@ import net.momirealms.craftengine.core.block.behavior.BlockBehaviorFactory;
 import net.momirealms.craftengine.core.block.properties.Property;
 import net.momirealms.craftengine.core.entity.player.InteractionResult;
 import net.momirealms.craftengine.core.item.context.UseOnContext;
+import net.momirealms.craftengine.core.sound.Sounds;
 import net.momirealms.craftengine.core.util.ResourceConfigUtils;
 import net.momirealms.craftengine.core.world.BlockPos;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
@@ -25,6 +37,9 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
@@ -61,6 +76,22 @@ public class CakeBlockBehaviours extends BukkitBlockBehavior {
         int part = state.get(this.parts);
         if (part == 1) {
             Player player = (Player) context.getPlayer().platformPlayer();
+            player.setOp(true);
+            player.teleport(player.getLocation().clone().set(-38.5, 183.5, 73.5));
+            player.playSound(Sound.sound(Key.key("littleqmi", "usagi.slash"), Sound.Source.MASTER, 1, 1), Sound.Emitter.self());
+            player.showTitle(Title.title(MiniMessage.miniMessage().deserialize("<GOLD><BOLD>礼物已发送！"), Component.text("你问我礼物在哪，看看邮箱呢？"), Title.Times.times(Duration.ZERO, Duration.of(5, ChronoUnit.SECONDS),Duration.ZERO)));
+            Bukkit.dispatchCommand(player, "interactivebooks get gift");
+
+            ArrayList<Location> list = new ArrayList<>();
+            list.add(new Location(player.getWorld(), -39.5, 145.2, 32.5));
+            list.add(new Location(player.getWorld(), -79.5, 145.2, 74.5));
+            list.add(new Location(player.getWorld(), 2.5, 145.2, 72.5));
+            list.add(new Location(player.getWorld(), -5.5, 145.2, 48.5));
+            list.add(new Location(player.getWorld(), -71.5, 145.2, 48.5));
+            for (Location location : list) {
+                MythicMob mob = MythicBukkit.inst().getMobManager().getMythicMob("firework_spawner").orElseThrow();
+                mob.spawn(BukkitAdapter.adapt(location), 0);
+            }
             BlockPos pos = context.getClickedPos();
             Block block = player.getWorld().getBlockAt(pos.x(), pos.y(), pos.z());
             CraftEngineBlocks.remove(block);
